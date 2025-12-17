cf58b83999bb175e74739a96f42ea53bd46db7eb
David Forst <dforst@g.jct.ac.il>
2025-12-17 13:16:02 +0200

    Making functions more general with a path argument

--- server2/main.py
+++ server2/main.py
@@ -7,6 +7,7 @@ import uvicorn
 app = FastAPI(title="Items API", version="1.0.0")
 
 DB_PATH = Path("db/shopping_list.json")
+BIND_PATH = Path("data/backup_shopping_list.json")
 
 
 class Item(BaseModel):
@@ -16,45 +17,46 @@ class Item(BaseModel):
 
 def check_database_exists(db_path: Path) -> None:
     if not db_path.exists():
-        with open(DB_PATH, "w") as f:
+        with open(db_path, "w") as f:
             json.dump([], f)
 
 
-def load_database() -> list[dict]:
+def load_database(json_path: Path) -> list[dict]:
     """Load data from JSON file and return as dictionary"""
     try:
-        with open(DB_PATH, "r", encoding="utf-8") as f:
+        with open(json_path, "r", encoding="utf-8") as f:
             return json.load(f)
     except json.JSONDecodeError:
         raise ValueError("Database file is not valid JSON.")
 
 
-def save_database(data: list[dict]) -> None:
+def save_database(path: Path, data: list[dict]) -> None:
     """Save dictionary data to JSON file"""
-    with open(DB_PATH, "w") as f:
+    with open(path, "w") as f:
         json.dump(data, f)
 
 
 @app.on_event("startup")
 async def startup_event():
     check_database_exists(DB_PATH)
+    check_database_exists(BIND_PATH)
 
 
 @app.get("/items/")
 async def list_items():
-    return load_database()
+    return load_database(DB_PATH)
 
 
 @app.post("/items/")
 async def create_item(item: Item):
-    items = load_database()
+    items = load_database(DB_PATH)
     ids = [int(k["id"]) for k in items]
     new_id = str(max(ids) + 1) if ids else "1"
     item_dict = {"id": new_id}
     item_dict.update(item.dict())
     items.append(item_dict)
 
-    save_database(items)
+    save_database(DB_PATH, items)
     return {
         "message": "Item created successfully",
         "item": {
